5911125bd74f6f41f677cf67c2d22e48a5036e23
ApseliSyifa <syifaapseli@gmail.com>
2021-11-11 21:12:01 +0700

    first commit

---
+++ CRUDMsAccess/Form1.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace CRUDMsAccess
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnCari = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.employeeIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.namaBarangDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.jumlahBarangDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hargaBarangDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.imageDataGridViewImageColumn = new System.Windows.Forms.DataGridViewImageColumn();
+            this.employeesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.appData = new CRUDMsAccess.AppData();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel = new System.Windows.Forms.Panel();
+            this.txtHargaBarang = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtJumlahBarang = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNamaBarang = new System.Windows.Forms.TextBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.txtCariBarang = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.btnKembali = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnBaru = new System.Windows.Forms.Button();
+            this.employeesTableAdapter = new CRUDMsAccess.AppDataTableAdapters.EmployeesTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.appData)).BeginInit();
+            this.panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCari
+            // 
+            this.btnCari.BackColor = System.Drawing.Color.White;
+            this.btnCari.ForeColor = System.Drawing.Color.Black;
+            this.btnCari.Location = new System.Drawing.Point(48, 194);
+            this.btnCari.Name = "btnCari";
+            this.btnCari.Size = new System.Drawing.Size(79, 25);
+            this.btnCari.TabIndex = 0;
+            this.btnCari.Text = "Cari";
+            this.btnCari.UseVisualStyleBackColor = false;
+            this.btnCari.Click += new System.EventHandler(this.btnCari_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AutoGenerateColumns = false;
+            this.dataGridView.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.employeeIDDataGridViewTextBoxColumn,
+            this.namaBarangDataGridViewTextBoxColumn,
+            this.jumlahBarangDataGridViewTextBoxColumn,
+            this.hargaBarangDataGridViewTextBoxColumn,
+            this.imageDataGridViewImageColumn});
+            this.dataGridView.DataSource = this.employeesBindingSource;
+            this.dataGridView.Location = new System.Drawing.Point(28, 287);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(549, 234);
+            this.dataGridView.TabIndex = 1;
+            this.dataGridView.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridView_KeyDown);
+            // 
+            // employeeIDDataGridViewTextBoxColumn
+            // 
+            this.employeeIDDataGridViewTextBoxColumn.DataPropertyName = "EmployeeID";
+            this.employeeIDDataGridViewTextBoxColumn.HeaderText = "EmployeeID";
+            this.employeeIDDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.employeeIDDataGridViewTextBoxColumn.Name = "employeeIDDataGridViewTextBoxColumn";
+            this.employeeIDDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // namaBarangDataGridViewTextBoxColumn
+            // 
+            this.namaBarangDataGridViewTextBoxColumn.DataPropertyName = "NamaBarang";
+            this.namaBarangDataGridViewTextBoxColumn.HeaderText = "NamaBarang";
+            this.namaBarangDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.namaBarangDataGridViewTextBoxColumn.Name = "namaBarangDataGridViewTextBoxColumn";
+            this.namaBarangDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // jumlahBarangDataGridViewTextBoxColumn
+            // 
+            this.jumlahBarangDataGridViewTextBoxColumn.DataPropertyName = "JumlahBarang";
+            this.jumlahBarangDataGridViewTextBoxColumn.HeaderText = "JumlahBarang";
+            this.jumlahBarangDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.jumlahBarangDataGridViewTextBoxColumn.Name = "jumlahBarangDataGridViewTextBoxColumn";
+            this.jumlahBarangDataGridViewTextBoxColumn.Width = 120;
+            // 
+            // hargaBarangDataGridViewTextBoxColumn
+            // 
+            this.hargaBarangDataGridViewTextBoxColumn.DataPropertyName = "HargaBarang";
+            this.hargaBarangDataGridViewTextBoxColumn.HeaderText = "HargaBarang";
+            this.hargaBarangDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.hargaBarangDataGridViewTextBoxColumn.Name = "hargaBarangDataGridViewTextBoxColumn";
+            this.hargaBarangDataGridViewTextBoxColumn.Width = 130;
+            // 
+            // imageDataGridViewImageColumn
+            // 
+            this.imageDataGridViewImageColumn.DataPropertyName = "Image";
+            this.imageDataGridViewImageColumn.HeaderText = "Image";
+            this.imageDataGridViewImageColumn.MinimumWidth = 6;
+            this.imageDataGridViewImageColumn.Name = "imageDataGridViewImageColumn";
+            this.imageDataGridViewImageColumn.Width = 120;
+            // 
+            // employeesBindingSource
+            // 
+            this.employeesBindingSource.DataMember = "Employees";
+            this.employeesBindingSource.DataSource = this.appData;
+            // 
+            // appData
+            // 
+            this.appData.DataSetName = "AppData";
+            this.appData.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(194, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nama Barang";
+            // 
+            // panel
+            // 
+            this.panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panel.Controls.Add(this.txtHargaBarang);
+            this.panel.Controls.Add(this.label3);
+            this.panel.Controls.Add(this.txtJumlahBarang);
+            this.panel.Controls.Add(this.label2);
+            this.panel.Controls.Add(this.txtNamaBarang);
+            this.panel.Controls.Add(this.pictureBox);
+            this.panel.Controls.Add(this.btnCari);
+            this.panel.Controls.Add(this.label1);
+            this.panel.Enabled = false;
+            this.panel.Location = new System.Drawing.Point(28, 15);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(695, 234);
+            this.panel.TabIndex = 0;
+            // 
+            // txtHargaBarang
+            // 
+            this.txtHargaBarang.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.employeesBindingSource, "HargaBarang", true));
+            this.txtHargaBarang.Location = new System.Drawing.Point(316, 104);
+            this.txtHargaBarang.Name = "txtHargaBarang";
+            this.txtHargaBarang.Size = new System.Drawing.Size(203, 22);
+            this.txtHargaBarang.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(194, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Harga Barang";
+            // 
+            // txtJumlahBarang
+            // 
+            this.txtJumlahBarang.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.employeesBindingSource, "JumlahBarang", true));
+            this.txtJumlahBarang.Location = new System.Drawing.Point(316, 65);
+            this.txtJumlahBarang.Name = "txtJumlahBarang";
+            this.txtJumlahBarang.Size = new System.Drawing.Size(203, 22);
+            this.txtJumlahBarang.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(194, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Jumlah Barang";
+            // 
+            // txtNamaBarang
+            // 
+            this.txtNamaBarang.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.employeesBindingSource, "NamaBarang", true));
+            this.txtNamaBarang.Location = new System.Drawing.Point(316, 26);
+            this.txtNamaBarang.Name = "txtNamaBarang";
+            this.txtNamaBarang.Size = new System.Drawing.Size(203, 22);
+            this.txtNamaBarang.TabIndex = 0;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox.DataBindings.Add(new System.Windows.Forms.Binding("Image", this.employeesBindingSource, "Image", true));
+            this.pictureBox.Location = new System.Drawing.Point(11, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(149, 170);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // txtCariBarang
+            // 
+            this.txtCariBarang.Location = new System.Drawing.Point(156, 551);
+            this.txtCariBarang.Name = "txtCariBarang";
+            this.txtCariBarang.Size = new System.Drawing.Size(421, 22);
+            this.txtCariBarang.TabIndex = 1;
+            this.txtCariBarang.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCariBarang_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(36, 553);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Cari Barang";
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.BackColor = System.Drawing.Color.White;
+            this.btnSimpan.ForeColor = System.Drawing.Color.Black;
+            this.btnSimpan.Location = new System.Drawing.Point(626, 486);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(98, 35);
+            this.btnSimpan.TabIndex = 5;
+            this.btnSimpan.Text = "Simpan";
+            this.btnSimpan.UseVisualStyleBackColor = false;
+            this.btnSimpan.Click += new System.EventHandler(this.btnSimpan_Click);
+            // 
+            // btnKembali
+            // 
+            this.btnKembali.BackColor = System.Drawing.Color.White;
+            this.btnKembali.ForeColor = System.Drawing.Color.Black;
+            this.btnKembali.Location = new System.Drawing.Point(625, 419);
+            this.btnKembali.Name = "btnKembali";
+            this.btnKembali.Size = new System.Drawing.Size(98, 35);
+            this.btnKembali.TabIndex = 4;
+            this.btnKembali.Text = "Kembali";
+            this.btnKembali.UseVisualStyleBackColor = false;
+            this.btnKembali.Click += new System.EventHandler(this.btnKembali_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackColor = System.Drawing.Color.White;
+            this.btnEdit.ForeColor = System.Drawing.Color.Black;
+            this.btnEdit.Location = new System.Drawing.Point(622, 350);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(102, 35);
+            this.btnEdit.TabIndex = 3;
+            this.btnEdit.Text = "Update";
+            this.btnEdit.UseVisualStyleBackColor = false;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnBaru
+            // 
+            this.btnBaru.BackColor = System.Drawing.Color.White;
+            this.btnBaru.ForeColor = System.Drawing.Color.Black;
+            this.btnBaru.Location = new System.Drawing.Point(622, 287);
+            this.btnBaru.Name = "btnBaru";
+            this.btnBaru.Size = new System.Drawing.Size(101, 35);
+            this.btnBaru.TabIndex = 2;
+            this.btnBaru.Text = "Baru";
+            this.btnBaru.UseVisualStyleBackColor = false;
+            this.btnBaru.Click += new System.EventHandler(this.btnBaru_Click);
+            // 
+            // employeesTableAdapter
+            // 
+            this.employeesTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(757, 605);
+            this.Controls.Add(this.btnBaru);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnKembali);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.txtCariBarang);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.dataGridView);
+            this.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BetaMart";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.appData)).EndInit();
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCari;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.TextBox txtNamaBarang;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TextBox txtHargaBarang;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtJumlahBarang;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCariBarang;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.Button btnKembali;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnBaru;
+        private AppData appData;
+        private System.Windows.Forms.BindingSource employeesBindingSource;
+        private AppDataTableAdapters.EmployeesTableAdapter employeesTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn employeeIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn namaBarangDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn jumlahBarangDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hargaBarangDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewImageColumn imageDataGridViewImageColumn;
+    }
+}
+
